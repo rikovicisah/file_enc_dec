af0d7197079f75ffb5306abc712a16570a0b2777
Isah Rikovic <rikovicisah@gmail.com>
2025-06-24 23:02:31 +0200

    Initial commit - descr.

---
+++ enkriptor.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<sodium.h>
+
+/*
+@author: Isah R i Adnan D.
+@date: 24.6.2025
+@name: File encryptor/decryptor
+@description: Encrypts/Decrypts file of files in folder
+*/
+
+//program ce moci da enkriptuje i dekriptuje fajlove 
+//moci ce da enkriptuje i dekriptuje kako pojedinacne fajlove tako i fajlove u folderu 
+//unijet ce se kljuc koji ce koristiti da enkripciju i dekripciju
+
+// -e argument ce biti za enkripciju -d za dekripciju fajla ili fajlova
+// -k za prosljedjivanje kljuca 
+
+//ako se enkriptuje folder tj sadrzaj foldera, fajlovi ce po defaultu imati nastavak _e i (1)(2)...(n) 
+//pa ce ime fajla biti ime_e(1) itd...
+
+int main(int argc, char *argv[]){
+
+
+//tri argumenta jer prvi je ime programa i dva fajla, 
+//prvi fajl kako se zove i drugi fajl kako ce se zvati nakon enkripcije
+
+
+    return 0;
+}
